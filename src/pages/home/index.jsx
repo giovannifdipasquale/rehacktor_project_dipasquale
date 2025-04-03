afa0fe86ddd727cc833c46acf6d990be93324e10
giovanni <giovanni@MacBook-Air-di-giovanni.local>
2025-04-03 16:25:55 +0200

    07-import libreria supabase, creazione client con url e chiave api

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import useFetchSolution from "../../hook/useFetchSolution";
 import Card from "../../components/Card";
 function HomePage() {
-  // 9ca1f1c570a64e2aac287f86910378f8
   const { data, error } = useFetchSolution(
     `https://api.rawg.io/api/games?key=9ca1f1c570a64e2aac287f86910378f8&dates=2024-01-01,2024-12-31&page=1`
   );
